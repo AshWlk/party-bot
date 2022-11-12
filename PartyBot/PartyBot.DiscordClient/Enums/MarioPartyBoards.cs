28ec23b7854e0e0eb0e28c2edaca1ed0a6fca2e7
Ashley Walker <ashwlk@outlook.com>
2022-11-12 19:08:18 +0000

    Add Mario Party 1 & 2 boards

--- PartyBot/PartyBot.DiscordClient/Enums/MarioPartyBoards.cs
+++ PartyBot/PartyBot.DiscordClient/Enums/MarioPartyBoards.cs
@@ -17,6 +17,36 @@ namespace PartyBot.DiscordClient.Enums
         WoodyWoods,
 
         [Description("Horror Land")]
-        HorrorLand
+        HorrorLand,
+
+        [Description("DK's Jungle Adventure")]
+        DKsJungleAdventure,
+
+        [Description("Wario's Battle Canyon")]
+        WariosBattleCanyon,
+
+        [Description("Luigi's Engine Room")]
+        LuigisEngineRoom,
+
+        [Description("Mario's Rainbow Castle")]
+        MariosRainbowCastle,
+
+        [Description("Bowser's Magma Mountain")]
+        BowsersMagmaMountain,
+
+        [Description("Eternal Star")]
+        EternalStar,
+
+        [Description("Pirate Land")]
+        PirateLand,
+
+        [Description("Western Land")]
+        WesternLand,
+
+        [Description("Mystery Land")]
+        MysteryLand,
+
+        [Description("Bowser Land")]
+        BowserLand
     }
 }
