f9a7529acab03605c223a30d8c895c4ad41a6939
Ashley Walker <AshWlk@outlook.com>
2022-11-26 14:41:46 +0000

    New project for parsing screen content

---
+++ PartyBot/PartyBot.ScreenParser.Console/Program.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System.Runtime.Versioning;
+
+namespace PartyBot.ScreenParser.Console
+{
+    [SupportedOSPlatform("Windows")]
+    public static class Program
+    {
+        public static async Task Main()
+        {
+            await Host
+                .CreateDefaultBuilder()
+                .UseConsoleLifetime()
+                .ConfigureAppConfiguration(configBuilder =>
+                {
+                    configBuilder.SetBasePath(AppDomain.CurrentDomain.BaseDirectory);
+                    configBuilder.AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+                    configBuilder.AddJsonFile("appsettings.local.json", optional: true, reloadOnChange: true);
+                })
+                .ConfigureServices((context, services) =>
+                {
+                    services.AddHostedService<ScreenshotService>();
+                })
+                .RunConsoleAsync();
+        }
+    }
+}
+
+
---
+++ PartyBot/PartyBot.ScreenParser.Console/ScreenshotService.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Extensions.Hosting;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Runtime.Versioning;
+using Vanara.PInvoke;
+
+namespace PartyBot.ScreenParser.Console
+{
+    [SupportedOSPlatform("Windows")]
+    internal class ScreenshotService : IHostedService
+    {
+        private Timer? _timer;
+
+        public Task StartAsync(CancellationToken stoppingToken)
+        {
+            this._timer = new Timer(this.Execute, null, TimeSpan.Zero, TimeSpan.FromSeconds(1));
+
+            return Task.CompletedTask;
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            this._timer?.Change(Timeout.Infinite, Timeout.Infinite);
+
+            return Task.CompletedTask;
+        }
+
+        private void Execute(object? state)
+        {
+            _ = User32.GetWindowRect(User32.GetForegroundWindow(), out var rect);
+            using var bitmap = new Bitmap(rect.Width, rect.Height);
+            using var graphics = Graphics.FromImage(bitmap);
+
+            graphics.CopyFromScreen(new Point(rect.Left, rect.Top), Point.Empty, rect.Size);
+            bitmap.Save(@$"C:\\temp\{Guid.NewGuid()}.jpg", ImageFormat.Jpeg);
+        }
+    }
+}
